c2f6a0f44e54f6f1c42e08c8501df1becc1ab60c
Dhiman <dhimannayak003@gmail.com>
2024-05-05 22:38:46 +0530

    fix some problem and upload image

--- client/src/components/UploadImage.jsx
+++ client/src/components/UploadImage.jsx
@@ -1,13 +1,39 @@
 import React from "react";
 import axios from "axios";
+import {useWeb3Context} from "../contexts/useWeb3Context"
+import toast from "react-hot-toast"
+import { useNavigate } from "react-router-dom";
+
 function UploadImage() {
+  const navigateTo = useNavigate();
+
   const [file, setFile] = React.useState(null);
+  const {web3State} =useWeb3Context()
+  const {selectedAccount,contractInstance}=web3State;
+  if(!selectedAccount){
+    navigateTo("/");
+  }
+
+  console.log("gg",selectedAccount,contractInstance);
+  const uploadImageHash = async (selectedAccount,ipfsHash)=>{
+    console.log(selectedAccount,ipfsHash);
+    const tx= await contractInstance.uploadFiles(selectedAccount,ipfsHash)
+    console.log("tx",tx);
+  }
+
   const handleImageUpload = async () => {
-    const formData = new FormData();
-    formData.append("file", file);
-    const url = `http://localhost:3000/api/uploadImage`;
-    const res = await axios.post(url, formData);
-    console.log(res.data.ipfsHash);
+    try {
+      const formData = new FormData();
+      formData.append("file", file);
+      const url = `http://localhost:3000/api/uploadImage`;
+      const res = await axios.post(url, formData);
+      console.log("ipfsHash",res.data.ipfsHash);
+      await uploadImageHash(selectedAccount,res.data.ipfsHash)
+      toast.success("image uploaded")
+    } catch (error) {
+      console.log(error);
+      toast.error("Image upload failed")
+    }
   };
   console.log(file);
   return (
--- client/src/pages/Wallet.jsx
+++ client/src/pages/Wallet.jsx
@@ -1,29 +1,32 @@
-import { useWeb3Context } from '../contexts/useWeb3Context'
-import { connectWallet } from '../utils/connectWallet'; 
-import { useEffect } from 'react';
-import {useNavigate } from "react-router-dom"
+import { useWeb3Context } from "../contexts/useWeb3Context";
+import { connectWallet } from "../utils/connectWallet";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 function Wallet() {
-
-  const navigateTo=useNavigate()
-    const {updateWeb3State,web3State}=useWeb3Context();
-    const {selectedAccount}=web3State;
-    useEffect(()=>{
-      if(selectedAccount){
-        navigateTo("/home")
-      }
-    },[selectedAccount,navigateTo])
-
-    const handleWalletConnection= async()=>{
-      const {contractInstance,selectedAccount}= await connectWallet()
-      updateWeb3State({contractInstance,selectedAccount});
+  const navigateTo = useNavigate();
+  const { updateWeb3State, web3State } = useWeb3Context();
+  const { selectedAccount } = web3State;
+  useEffect(() => {
+    if (selectedAccount) {
+      navigateTo("/home");
     }
-    
+  }, [selectedAccount, navigateTo]);
+
+  const handleWalletConnection = async () => {
+    try {
+      const { contractInstance, selectedAccount } = await connectWallet();
+      updateWeb3State({ contractInstance, selectedAccount });
+      navigateTo("/home");
+    } catch (error) {
+      console.error("Error connecting wallet:", error);
+    }
+  };
 
   return (
     <div>
       <button onClick={handleWalletConnection}>Connect</button>
     </div>
-  )
+  );
 }
 
-export default Wallet
+export default Wallet;
